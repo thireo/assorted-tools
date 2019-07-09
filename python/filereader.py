7e49fd8d29df63b4a03f1edb6bf2007a8eade9cb
ATHIR-MCBK-UBUNTU <at@danishcare.dk>
2019-07-09 21:28:48 +0200

    Fixed filereader. Needs tests with larger payloads. Doesn't currently support multiple date directories.

--- python/filereader.py
+++ python/filereader.py
@@ -8,15 +8,17 @@ cc = 0
 cc0 = 0
 cc1 = 0
 
+
 def num_there(s):
     return any(i.isdigit() for i in s)
 
-collData = []
+
+collData = {}
 for date in os.listdir(path):
     if(os.path.isdir(path+date) and date != "firmware" and date.__contains__("2018_11_21")):
         print(date)
         currentPackage = 0
-        
+        tempData = []
         for file in os.listdir(path+date):
             if(file.__contains__(".bin")):
                 with open(path+date+"/"+file, errors="ignore") as f:
@@ -25,35 +27,27 @@ for date in os.listdir(path):
                     for line in test:
                         splitLine = line.split(" ")
                         if(splitLine[0].__contains__("Package")):
-                            #print(splitLine.__len__())
-                            #print(int(splitLine[2]))
                             currentPackage = int(splitLine[2])
                         splitLine = line.split("\t")
                         if(splitLine.__len__() > 2):
-                            #print(bob.__len__())
-                            #print(bob)
-                            cc0+=1
+                            cc0 += 1
                         elif(splitLine.__len__() == 2 and splitLine[0].__contains__("/")):
-                            #with int(splitLine[1],base=10) as temp:
-                            #    tt = temp
-                            #    print(tt)
                             if(num_there(splitLine[1])):
                                 data.append(int(splitLine[1]))
-                                collData[currentPackage].append(int(splitLine[1]))
                                 cc1 += 1
-                        else: 
-                            cc += 1
-                    #print(test[5:7])
-                    #print(file)
-                    #cc += test.__len__()
-                    #collData[0][:] = data
-                    
-                #count+=1
-        #print(cc0,"/",cc1," - ",cc)
+                            else:
+                                cc += 1
+                    collData[currentPackage] = data
         count = 0
         cc = 0
         cc0 = 0
         cc1 = 0
-print(collData)
-plt.plot(collData)
+
+tempo = []
+print(collData.__len__())
+print(range(collData.__len__()))
+for i in range(collData.__len__()):
+    tempo.extend(collData[collData.__len__()-i-1])
+
+plt.plot(tempo)
 plt.show()
