14b8fb574c67a6a946eccd7f98202e4ea837fa24
Andreas <dk.andreas@gmail.com>
2019-07-11 10:09:34 +0200

    Added Missing Data handling.

--- python/filereader.py
+++ python/filereader.py
@@ -13,13 +13,15 @@ def num_there(s):
     return any(i.isdigit() for i in s)
 
 
-collData = {}
+
 for date in sorted(os.listdir(path)):
-    if(os.path.isdir(path+date) and date != "firmware" and date.__contains__("2019_07_08")):
+    if(os.path.isdir(path+date) and date != "firmware"): #and date.__contains__("2019_07_01")):
         print(date)
         currentPackage = 0
         tempData = []
+        collData = {}
         for file in sorted(os.listdir(path+date)):
+            
             if(file.__contains__(".bin")):
                 with open(path+date+"/"+file, errors="ignore") as f:
                     data = []
@@ -39,22 +41,25 @@ for date in sorted(os.listdir(path)):
                             else:
                                 cc += 1
                 collData[currentPackage] = data
-                print(collData.__len__()," - ",currentPackage)
+                #print(collData.__len__()," - ",currentPackage)
+                if(collData.__len__() < currentPackage):
+                    print("Missing Data ERROR")
+                    break
                 if(currentPackage == 0 and collData.__len__()>1):
                     #print(collData.__len__())
-                    print("bob")
+                    #print("bob")
                     for i in range(len(collData)):
                         #print(collData[collData.__len__()-1-i])
                         tempData.extend(collData[collData.__len__()-i-1])
                     collData = {}
                     currentPackage = 0
 
-        print(cc1)
+        #print(cc1)
         count = 0
         cc = 0
         cc0 = 0
         cc1 = 0
-        print(collData.__len__())
+        #print(collData.__len__())
         #print(collData)
         #tempo = []
         #for i in range(.__len__()):
