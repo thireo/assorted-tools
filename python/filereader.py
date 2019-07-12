d627c68d8507f5576dd516b11d95a04312caf457
Andreas <dk.andreas@gmail.com>
2019-07-12 10:39:52 +0200

    Added more data files for testing.
    filereader.py no longer saves data from dates with missing data

--- python/filereader.py
+++ python/filereader.py
@@ -1,70 +1,80 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-path = "data/"
-count = 0
-cc = 0
-cc0 = 0
-cc1 = 0
-
+#path = "data/"
 
 def num_there(s):
     return any(i.isdigit() for i in s)
 
 
+def process(path,plot):
+    for date in sorted(os.listdir(path)):
+        if(os.path.isdir(path+date) and date != "firmware"): #and date.__contains__("2019_07_01")):
+            errorOccurred = False
+            print(date)
+            currentPackage = 0
+            tempData = []
+            collData = {}
+            for file in sorted(os.listdir(path+date)):
+                
+                if(file.__contains__(".bin")):
+                    with open(path+date+"/"+file, errors="ignore") as f:
+                        data = []
+                        test = f.readlines()
+                        for line in test:
+                            splitLine = line.split(" ")
+                            if(splitLine[0].__contains__("Package")):
+                                currentPackage = int(splitLine[2])
+                                #print(currentPackage)
+                            splitLine = line.split("\t")
+                            if(splitLine.__len__() > 2):
+                                continue
+                            elif(splitLine.__len__() == 2 and splitLine[0].__contains__("/")):
+                                if(num_there(splitLine[1])):
+                                    data.append(int(splitLine[1]))
+                    collData[currentPackage] = data
+                    #print(collData.__len__()," - ",currentPackage)
+                    if(collData.__len__() < currentPackage):
+                        print(date,"Missing Data ERROR")
+                        errorOccurred = True
+                        break
+                    if(currentPackage == 0 and collData.__len__()>1):
+                        #print(collData.__len__())
+                        #print("bob")
+                        for i in range(len(collData)):
+                            #print(collData[collData.__len__()-1-i])
+                            tempData.extend(collData.get(collData.__len__()-i-1,[]))
+                        collData = {}
+                        currentPackage = 0
+            #print(collData.__len__())
+            #print(collData)
+            #tempo = []
+            #for i in range(.__len__()):
+            #    tempo.extend(collData[collData.__len__()-i-1])
+            if(not errorOccurred):
+                f = open(date,"w+")
+                for line in tempData:
+                    f.write("%d\n"%line)
+                f.close()
+                #if(plot):
+                    #plt.plot(path,"/",tempData)
+                    #plt.show()
+            else:
+                if(os._exists((path,"/",date))):
+                    os.remove(path,"/",date)
+            #print(tempData)
+            #print(collData[9])
 
-for date in sorted(os.listdir(path)):
-    if(os.path.isdir(path+date) and date != "firmware"): #and date.__contains__("2019_07_01")):
-        print(date)
-        currentPackage = 0
-        tempData = []
-        collData = {}
-        for file in sorted(os.listdir(path+date)):
-            
-            if(file.__contains__(".bin")):
-                with open(path+date+"/"+file, errors="ignore") as f:
-                    data = []
-                    test = f.readlines()
-                    for line in test:
-                        splitLine = line.split(" ")
-                        if(splitLine[0].__contains__("Package")):
-                            currentPackage = int(splitLine[2])
-                            #print(currentPackage)
-                        splitLine = line.split("\t")
-                        if(splitLine.__len__() > 2):
-                            cc0 += 1
-                        elif(splitLine.__len__() == 2 and splitLine[0].__contains__("/")):
-                            if(num_there(splitLine[1])):
-                                data.append(int(splitLine[1]))
-                                cc1 += 1
-                            else:
-                                cc += 1
-                collData[currentPackage] = data
-                #print(collData.__len__()," - ",currentPackage)
-                if(collData.__len__() < currentPackage):
-                    print("Missing Data ERROR")
-                    break
-                if(currentPackage == 0 and collData.__len__()>1):
-                    #print(collData.__len__())
-                    #print("bob")
-                    for i in range(len(collData)):
-                        #print(collData[collData.__len__()-1-i])
-                        tempData.extend(collData[collData.__len__()-i-1])
-                    collData = {}
-                    currentPackage = 0
+def main(argv):
+    import argparse
+    parser = argparse.ArgumentParser(description="Does stuff")
+    parser.add_argument("-d", "--directory", dest='path',default= '', required=True,help='filenamo')
+    parser.add_argument("-p","--plot", dest="plot",default=False,required=False,help='PLOOTT')
+    #parser.add_argument("-v","--verbose", dest="verbose",default=False,required=False,help='print everything')
+    args = parser.parse_args(argv)
+    process(args.path,False)
 
-        #print(cc1)
-        count = 0
-        cc = 0
-        cc0 = 0
-        cc1 = 0
-        #print(collData.__len__())
-        #print(collData)
-        #tempo = []
-        #for i in range(.__len__()):
-        #    tempo.extend(collData[collData.__len__()-i-1])
-        plt.plot(tempData)
-        plt.show()
-        #print(tempData)
-        #print(collData[9])
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
